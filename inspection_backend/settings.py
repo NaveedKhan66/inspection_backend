f6f247ceb8a4260e4d66800d49d91abff304fbf5
Naveed Khan <naveedkhan@tintash.com>
2024-05-12 18:20:36 +0500

    Feat: Add auto-admin creation
    Add and configure logger for the project

--- inspection_backend/settings.py
+++ inspection_backend/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inspection_backend.settings")
     try:
         from django.core.management import execute_from_command_line
-        from users import User
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -17,10 +17,6 @@ def main():
         ) from exc
 
     execute_from_command_line(sys.argv)
-    ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
-    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
-    if not User.objects.filter(email=ADMIN_EMAIL):
-        User.objects.create_superuser("admin", ADMIN_EMAIL, ADMIN_PASSWORD)
 
 
 if __name__ == "__main__":
