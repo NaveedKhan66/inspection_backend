c97d5d13351ba5420ef92eb8a7105b982f68fc54
Naveed Khan <naveedkhan@tintash.com>
2024-05-22 00:05:07 +0500

    Feat: Add builder, trade and client models
    Remove ExtendedChoice class for USER_TYPES and use generic types as user_types

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from users.models import User
+from users.models import Builder
+from users.models import Trade
+from users.models import Client
 
 # Register your models here.
 
@@ -9,3 +12,24 @@ class UserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+
+class BuilderAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "vbn"]
+
+
+admin.site.register(Builder, BuilderAdmin)
+
+
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "builder"]
+
+
+admin.site.register(Trade, TradeAdmin)
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ["id", "user"]
+
+
+admin.site.register(Client, ClientAdmin)
