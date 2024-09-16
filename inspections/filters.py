29da0fd472ab4348ac36d72f185e8ac39f94f7ce
Naveed Khan <naveedkhan@tintash.com>
2024-09-16 12:44:25 +0500

    Feature: add search filter on deficiencies based on the id of deficiency

--- inspections/filters.py
+++ inspections/filters.py
@@ -11,6 +11,7 @@ class DeficiencyFilter(django_filters.FilterSet):
     location = django_filters.CharFilter(lookup_expr="icontains")
     status = django_filters.ChoiceFilter(choices=Deficiency.STATUS_TYPES)
     trade = django_filters.UUIDFilter(field_name="trade__id")
+    search = django_filters.CharFilter(method="filter_search")
 
     class Meta:
         model = Deficiency
@@ -22,3 +23,13 @@ class DeficiencyFilter(django_filters.FilterSet):
             "status",
             "trade",
         ]
+
+    def filter_search(self, queryset, name, value):
+        # Ensure the search value contains only digits
+        if not value.isdigit():
+            return (
+                queryset.none()
+            )  # Return an empty queryset if the input is not numeric
+
+        # Convert the ID field to string for partial matching
+        return queryset.filter(id__icontains=value)
